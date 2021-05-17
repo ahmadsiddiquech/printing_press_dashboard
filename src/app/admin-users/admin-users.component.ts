332395c2227902f4a28545ba54263122b81c3275
Ahmad Siddique <ahmadsiddiquech@gmail.com>
2021-05-17 19:53:43 +0500

    user service

--- src/app/admin-users/admin-users.component.ts
+++ src/app/admin-users/admin-users.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UsersService } from '../common/services/users.service';
 
 @Component({
   selector: 'app-admin-users',
@@ -6,10 +7,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./admin-users.component.css']
 })
 export class AdminUsersComponent implements OnInit {
+  user_data: any;
 
-  constructor() { }
+  constructor(private userService : UsersService) { }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(){
+      this.userService.getUsers()
+      .subscribe(
+        response => {
+          this.user_data = response;
+          if(this.user_data.success){
+            this.user_data = this.user_data.data;
+            this.user_data.image = (this.user_data.image == null) ? '../../assets/images/user_image.png' : 'uploads/images/'+this.user_data.image;
+          }
+        },
+        error => {
+          console.log(error)
+        }
+      )
+    }
 
 }
--- src/app/common/services/users.service.ts
+++ src/app/common/services/users.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsersService {
 
-  constructor() { }
+  private usersUrl = `http://localhost:3000/api/users`;
+  constructor(private http:HttpClient) { }
+
+  getUsers(){
+    return this.http.get(this.usersUrl);
+  }
+
+  getUser(id:any){
+    return this.http.get(this.usersUrl+'/'+id);
+  }
+
+  updateUsers(id:any,user:any){
+    return this.http.put(this.usersUrl+'/'+id,user);
+  }
+
+  updateUsersImage(id:any,user:any){
+    return this.http.put(this.usersUrl+'/upload_image/'+id,user);
+  }
 }
